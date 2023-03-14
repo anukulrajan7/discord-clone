85af82f9205d9d7f35b98d6fbbdbb0b358b29f5c
Anukul <anukulraj2004@gmail.com>
2023-03-14 08:22:26 +0530

    push

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+
+      "bg-blue":"#0000ee",
+      "bg-sky":"#00b0f4",
+ "bg-mily":"#f6f6f6",
+ "bg-whites":"#ffffff",
+"bg-begani":"#404eed",
+"bg-light-begani":"#5865f2",
+
+    },
+    fontFamily:{
+      "sans": 'Roboto',
+    }
+  },
+  plugins: [],
+}}
